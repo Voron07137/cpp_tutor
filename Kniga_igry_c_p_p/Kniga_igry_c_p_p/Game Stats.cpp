9a82708551b6720366a120665e96c637c8f679ca
Voron07137 <oleg07137@gmail.com>
2017-07-25 01:45:53 +0300

    Updated 25/07/2017 1:45

---
+++ Kniga_igry_c_p_p/Kniga_igry_c_p_p/Game Stats.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int score = 0;
+//	double distance = 1200.76;
+//	char playAgain = 'y';
+//	bool shieldsUp = true;
+//	short lives = 3;
+//	short aliensKilled = 10;
+//	double engineTemp = 6572.89;
+//	cout << "\nscore: " << score << endl;
+//	cout << "distance: " << distance << endl;
+//	cout << "playAgain: " << playAgain << endl;
+//	cout << "lives: " << lives << endl;
+//	cout << "aliensKilled: " << aliensKilled << endl;
+//	cout << "engineTemp: " << engineTemp << endl;
+//	int fuel;
+//	cout << "\nHow much fuel? ";
+//	cin >> fuel;
+//	cout << "fuel: " << fuel << endl;
+//	typedef unsigned short int ushort;
+//	ushort bonus = 10;
+//	cout << "\nbonus: " << bonus << endl;
+//	return 0;
+//}
---
+++ Kniga_igry_c_p_p/Kniga_igry_c_p_p/GameOver 2.0.cpp
@@ -0,0 +1,7 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "game over!" << endl;
+//	return 0;
+//}
